e891c2a30148e5bfdf5c66021973032e16621807
hanlacey <hnnhlcy@gmail.com>
2021-03-31 17:00:34 +0100

    link to /api/students

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "@reach/router";
+import Button from "";
+const Home = () => {
+  return (
+    <div>
+      <Link to="/api/students">Student list</Link>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Navbar(props) {
+    return (
+        <header className="navbar">
+            Northcoders Student Tracker
+        </header>
+    );
+}
+
+export default Navbar;
